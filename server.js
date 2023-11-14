d540447246cb45260f2bb38a0d0524bf42129ec7
Sebastian Tleye <stleye@ovloop.com>
2023-11-14 13:55:57 -0300

    Added files

---
+++ server.js
@@ -0,0 +1,14 @@
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
+    await sleep(5000);
+  }
+}
+
+main();
